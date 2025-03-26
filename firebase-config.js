7643187a4e2ac2f49a94573c53bf7b5e93446d12
lfcsd <iraspired@gmail.com>
2025-03-26 16:35:44 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyABC...",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123...:web:abc..."
+};
+
+firebase.initializeApp(firebaseConfig);
