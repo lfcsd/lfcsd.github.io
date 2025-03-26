a905bd6fea7bbe1a554954c12f0071369633feea
lfcsd <iraspired@gmail.com>
2025-03-26 16:39:15 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyABC...",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123...:web:abc..."
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyB6yxt2JX4ubnFsiYf2stfdnHeqjNySiJc",
+    authDomain: "lfcsd-days.firebaseapp.com",
+    projectId: "lfcsd-days",
+    storageBucket: "lfcsd-days.firebasestorage.app",
+    messagingSenderId: "520576481150",
+    appId: "1:520576481150:web:b08a50be7b0d15e113e52f",
+    measurementId: "G-7NPR8M9T6C"
+  };
 
-firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
