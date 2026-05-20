9fd45c21c79519110b57d5e1c9771da8b575d8a0
lfcsd <iraspired@gmail.com>
2026-05-20 09:11:53 -0400

    Update script.js

--- new/script.js
+++ new/script.js
@@ -387,7 +387,9 @@ document.addEventListener('DOMContentLoaded', async function() {
   SPECIAL_DATES[key(2026, 3, 3)] = { type: "off", label: "Spring Recess" };
   addRange(SPECIAL_DATES, 2026, 3, 6, 10, "off", "Spring Recess");
   SPECIAL_DATES[key(2026, 4, 25)] = { type: "off", label: "No School - Holiday" };
+  SPECIAL_DATES[key(2026, 4, 22)] = { type: "off", label: "No School - Unused Snow Day" }; // May 22
   SPECIAL_DATES[key(2026, 5, 19)] = { type: "off", label: "No School - Holiday" };
+  
 
   SPECIAL_DATES[key(2025, 8, 19)] = { type: "half", label: "1/2 Day Students. Staff PD" };
   SPECIAL_DATES[key(2025, 9, 6)]  = { type: "half", label: "1/2 Day Students. Staff PD" };
